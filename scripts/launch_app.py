7127520c3539bd65c6241d6c4a36e3cb6bfe6195
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-03-09 09:30:21 +0000

    Allow running the app in debug mode from the command line

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python3
 """Launch the flask-forecaster application."""
 import os
+import sys
 
 from flask_forecaster import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT')))
+    app.run(
+        debug='debug' in sys.argv,
+        host='0.0.0.0',
+        port=int(os.getenv('PORT'))
+    )
