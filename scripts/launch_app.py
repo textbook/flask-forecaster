63d6fb766f7f8164e881cd58320d99f0f1931d7f
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-03-10 16:46:10 +0000

    Fix issue with launching app on CF

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -12,6 +12,10 @@ logging.basicConfig(
     stream=sys.stdout,
 )
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     from flask_forecaster import app
-    app.run()
+    host, port = app.config['SERVER_NAME'].split(':')
+    logger.info('starting app on %s, %s', host, port)
+    app.run(host=host, port=int(port))
