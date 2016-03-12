d260eefb8dc8ca8bc71c548c1389853e49eafd28
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-03-12 16:24:13 +0000

    Add logging to database management script

--- scripts/manage_db.py
+++ scripts/manage_db.py
@@ -1,5 +1,8 @@
 """Database management and migration functionality."""
 
+import logging
+import sys
+
 # pylint: disable=no-name-in-module,import-error
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -7,10 +10,20 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 from flask_forecaster.flask_app import app, db
 
+logging.basicConfig(
+    datefmt='%Y/%m/%d %H.%M.%S',
+    format='%(levelname)s : %(name)s : %(message)s',
+    level=logging.DEBUG,
+    stream=sys.stdout,
+)
+
+logger = logging.getLogger('manage_db')
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    logger.info('managing the database')
     manager.run()
